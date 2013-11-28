06840971d5efeb7267ed176efa214e0e1262c45d
John <JohnnyBoiNZ@gmail.com>
2013-11-28 23:14:59 +1300

    Updates

--- Networking/NetworkManager.cs
+++ Networking/NetworkManager.cs
@@ -1,1 +1,22 @@
 //NetworkManager
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkManager : MonoBehaviour {
+
+	public string PlayerName;
+	public static NetworkManager instance;
+	public List<Player> PlayerList = new List<Player>();
+	public Player MyPlayer;
+	public GameObject SpawnPlayer;
+	public Level CurLevel;
+	public List<Level> ListOfLevels = new List<Level>();
+
+	public int police = 0;
+
+	void Awake()
+	{
+		
+	}
+}
