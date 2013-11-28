85ad6ae4ac3b0ee5e919821568875f85888e5f08
Orangejuice724 <xXSniper1313Xx1313@gmail.com>
2013-11-28 21:27:11 +1030

    PlayerController Update

--- Networking/PlayerController.cs
+++ Networking/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour {
 
     public Transform clientCar;
     public Transform networkCar;
+    public Vector3 CurPos;
+    public Quaternion CurRot;
+    public List<Level> ListOfLevels = new List<Level>();
+    public bool isCop;
 
     void Awake()
     {
@@ -15,7 +19,14 @@ public class PlayerController : MonoBehaviour {
 
     void Start()
     {
-
+        if (networkView.isMine)
+        {
+            MyPlayer = NetworkManager.getPlayer(networkView.owner);
+            MyPlayer.manager = this;
+        }
+        clientCar.gameObject.SetActive(false);
+        networkCar.gameObject.SetActive(false);
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
@@ -23,13 +34,78 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-    public void doSomething()
+    void FixedUpdate()
     {
 
     }
 
-    public void doSomethingElse()
+    [RPC]
+    public void RequestPlayer(string Nameee)
     {
+        networkView.RPC("GiveMyPlayer", RPCMode.OthersBuffered, Nameee);
+    }
 
+    [RPC]
+    public void GiveMyPlayer(string n)
+    {
+        StartCoroutine(GivePlayer(n));
+    }
+
+    IEnumerator GivePlayer(string nn)
+    {
+        while (!NetworkManager.HasPlayer(nn))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        MyPlayer = NetworkManager.getPlayer(nn);
+        MyPlayer.manager = this;
+    }
+
+    //Sniper add car racing code here ;-; unless i find a better way to do it
+    //Will be added at this line here
+
+    public void Client_PlaySound(string GunName, Vector3 soundPoint)
+    {
+        networkView.RPC("Server_PlaySound", RPCMode.Others, GunName, soundPoint);
+    }
+
+    [RPC]
+    public void Server_PlaySound(string GunName, Vector3 soundPoint)
+    {
+        AudioSource.PlayClipAtPoint(WeaponManager.FindWeapon(GunName).gameObject.audio.clip, soundPoint);
+    }
+
+    [RPC]
+    void Spawn()
+    {
+        MyPlayer.isSpawned = true;
+        if (networkView.isMine)
+        {
+            clientCar.gameObject.SetActive(true);
+            networkCar.gameObject.SetActive(false);
+        }
+        else
+        {
+            clientCar.gameObject.SetActive(false);
+            networkCar.gameObject.SetActive(true);
+        }
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            CurPos = clientCar.position;
+            CurRot = clientCar.rotation;
+            stream.Serialize(ref CurPos);
+            stream.Serialize(ref CurRot);
+        }
+        else
+        {
+            stream.Serialize(ref CurPos);
+            stream.Serialize(ref CurRot);
+            networkCar.position = CurPos;
+            networkCar.rotation = CurRot;
+        }
     }
 }
