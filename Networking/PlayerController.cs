616dce56d737d2e0b5b7fadd7f66d4c4081fef89
Orangejuice724 <xXSniper1313Xx1313@gmail.com>
2013-11-28 20:42:21 +1030

    Updated PlayerController

--- Networking/PlayerController.cs
+++ Networking/PlayerController.cs
@@ -1,1 +1,35 @@
 //PlayerController
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform clientCar;
+    public Transform networkCar;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void doSomething()
+    {
+
+    }
+
+    public void doSomethingElse()
+    {
+
+    }
+}
